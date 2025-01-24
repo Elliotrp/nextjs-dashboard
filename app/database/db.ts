5cf7dd56e9c49359937f1f6d404ce95e14d108fe
Elliot <ellraypres@hotmail.co.uk>
2025-01-24 18:45:51 +0000

    Completed chapter 7

--- app/database/db.ts
+++ app/database/db.ts
@@ -1,7 +1,8 @@
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
+import { Database } from "sqlite";
 
-export async function openDb() {
+export async function openDb(): Promise<Database> {
 	return open({
 		filename: process.env.SQLITE_DATABASE ?? ":memory:",
 		driver: sqlite3.Database,
